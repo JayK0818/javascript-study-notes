1c066d69ba7fed65ad6b3f20edbb0256b1c84cc5
JayK0818 <1129594783@qq.com>
2024-09-10 21:55:00 +0800

    feat: 观察者模式

--- 设计模式/observer/index.js
+++ 设计模式/observer/index.js
@@ -1,5 +1,5 @@
 /**
- * @description 观察者模式 定义对象间的一种一对多的依赖关系, 当一个对象的状态发生改变时, 所有以来于它的对象
+ * @description 观察者模式 定义对象间的一种一对多的依赖关系, 当一个对象的状态发生改变时, 所有依赖于它的对象
  * 都将得到通知和更新
 */
 class Subject {
@@ -67,5 +67,42 @@ pub_sub.subscribe('run', run)
 
 pub_sub.fire('walk', 'walk...')
 // walk [ 'walk...' ]
-pub_sub.fire('run', 'hello runing')
-// run: [ 'hello runing' ]
+pub_sub.fire('run', 'hello runing');
+  // run: [ 'hello runing' ]
+
+// ------------------------ 观察者模式 ------------------
+// 事件发布与事件订阅相互依赖, 观察者有一个update方法, Dep会调用观察者的update方法
+  // 而发布订阅模式 有一个事件中心, 发布者并不知道有谁订阅了这个事件
+(function () {
+  class Dep {
+    constructor() {
+      this.subs = [] // 记录所有订阅者
+    }
+    addSub(sub) {
+      // 对象要有updatw方法
+      if (sub && typeof sub.update === 'function') {
+        this.subs.push(sub)
+      }
+    }
+    notify() {
+      this.subs.forEach(sub => {
+        sub.update()
+      })    
+    }
+  }
+
+  class Watcher {
+    update() {
+      console.log('update')
+    }
+  }
+  const dep = new Dep()
+
+  const w_1 = new Watcher()
+  const w_2 = new Watcher()
+
+  dep.addSub(w_1)
+  dep.addSub(w_2)
+
+  dep.notify()
+})();
