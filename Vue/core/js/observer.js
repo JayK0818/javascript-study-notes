df9a20aefe040115f70c907bb797c4b03c75bfb7
JayK0818 <1129594783@qq.com>
2024-09-11 22:04:31 +0800

    feat:mini-vue

--- Vue/core/js/observer.js
+++ Vue/core/js/observer.js
@@ -86,3 +86,166 @@
     console.log(msg, message)
   }
 })();
+
+
+// 实现一个mini-vue
+(function () {
+  class Observer {
+    constructor(data) {
+      this.walk(data)
+    }
+    walk(data) {
+      if (typeof data === 'object' && data !== null) {
+        Object.keys(data).forEach(key => {
+          this.defineReactive(data, key, data[key])
+        })
+      }
+    }
+    defineReactive(data, key, val) {
+      this.walk(val) // 如果val是对象的话也转换为响应式
+      Object.defineProperty(data, key, {
+        enumerable: true,
+        configurable: true,
+        get() {
+          console.log('get-val', val)
+          return val
+        },
+        set(newValue) {
+          if (newValue !== val) {
+            val = newValue
+            console.log('更新了', val)
+          }
+        }
+      })
+    }
+  }
+  const obj = {
+    message: 'hello world',
+    count: 10,
+    player: {
+      firstName: 'kyrie',
+      lastName: 'irving'
+    }
+  }
+  const observer = new Observer(obj);
+  console.log('observer:', obj)
+  obj.count += 1
+  console.log('count:', obj.count) // 11
+  class MiniVue {
+    constructor(options) {
+      this.$options = options ?? {}
+      const { data = {}, el } = options ?? {}
+      this.$data = data
+      this.$el = typeof el === 'string' ? document.querySelector(el) : (typeof el === 'object' ? el : null)
+      this._proxy(data)
+      new Observer(this.$data)
+    }
+    _proxy(data) {
+      Object.keys(data).forEach(key => {
+        Object.defineProperty(this, key, {
+          enumerable: true,
+          configurable: true, // writable, value / getter, setter 不能同时设置
+          get() {
+            return data[key]
+          },
+          set(newValue) {
+            if (newValue === this[key]) {
+              return
+            }
+            this[key] = newValue
+            console.log('我更新了')
+          }
+        })
+      })
+    }
+  }
+  // 编译器
+  class Compiler {
+    constructor(vm, el) {
+      this.vm = vm
+      this.el = el
+      this.compile(el)
+    }
+    // 编译html
+    compile(root) {
+      const childNodes = Array.from(root.childNodes ?? [])
+      childNodes.forEach(node => {
+        switch (node.nodeType) {
+          case 3:
+            this.compileText(node)
+            break
+          case 1:
+            this.compileElement(node)
+            break
+        }
+        if (node.childNodes && node.childNodes.length) {
+          this.compile(node)
+        }
+      })
+    }
+    // 是不是指令
+    isDirective(attrName) {
+      return attrName.startsWith('v-')
+    }
+    // 编译文本
+    compileText(node) {
+      const reg = /\{\{(.+?)\}\}/
+      const content = node.textContent
+      const res = content.match(reg)
+      if (res) {
+        // 匹配成功
+        const key = res[1].trim()
+        const val = this.vm[key]
+        this.textUpdater(node, val)
+      }
+    }
+    compileElement(node) {
+      const attrs = Array.from(node.attributes ?? [])
+      attrs.forEach(attr => {
+        const attrName = attr.name
+        if (this.isDirective(attrName)) {
+          const directiveValue = attr.value
+          const type = attrName.slice(2)
+          const val = this.vm[directiveValue]
+          switch (type) {
+            case 'text':
+              this.textUpdater(node, val)
+              break
+            case 'model':
+              this.modelUpdated(node, val)
+              break
+          }
+        }
+      })
+    }
+    textUpdater(node, val) {
+      node.textContent = val
+    }
+    modelUpdated(node, val) {
+      node.value = val
+    }
+  }
+  const vm = new MiniVue({
+    data: {
+      message: 'hello world',
+      count: 10,
+      player: {
+        firstName: 'kyrie',
+        lastName: 'irving'
+      },
+      msg: '我是input框默认值'
+    }
+  })
+  console.log('vm', vm)
+  const compiler = new Compiler(vm, document.querySelector('#mini-vue-app'))
+  /**
+   * vm MiniVue {
+      '$options': { data: { message: 'hello world', count: 10 } },
+      '$data': { message: 'hello world', count: 10 },
+      '$el': null,
+      message: [Getter/Setter],
+      count: [Getter/Setter]
+    }
+   * 
+  */
+})();
